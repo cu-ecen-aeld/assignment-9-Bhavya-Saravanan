4665c93664a865ed3cd673017a1386460ab44c72
Bhavya-Saravanan <bhsa3618@colorado.edu>
2025-10-26 21:00:29 -0600

    A9:resolved the error

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -10,21 +10,15 @@ AESDCHAR_VERSION  = 'c7ba21bf8e37db77c19ff72cda41e66353b59179'
 AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
 
 
-
 define AESDCHAR_INSTALL_TARGET_CMDS
-	
-    $(INSTALL) -d $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra
-    $(INSTALL) -m 0755 $(@D)/aesd-char-driver/*.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra
-
-    $(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar-start-stop.sh $(TARGET_DIR)/etc/init.d/S97aesdchar
-
-     
-
-    $(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/
-    $(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/
-    $(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8-buildroot/drivertest.sh $(TARGET_DIR)/usr/bin
-	
+	$(INSTALL) -d $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/*.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar-start-stop.sh $(TARGET_DIR)/etc/init.d/S97aesdchar
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load   $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment9-buildroot/drivertest.sh $(TARGET_DIR)/usr/bin
 endef
+
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
